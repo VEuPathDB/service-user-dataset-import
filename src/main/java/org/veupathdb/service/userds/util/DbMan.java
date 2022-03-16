8302fce88835be3c7d990d0d81bad0eb64b2e21f
Elizabeth <foxcapade@gmail.com>
2022-03-16 11:45:32 -0400

    attempt to fix issue with #11

--- src/main/java/org/veupathdb/service/userds/util/DbMan.java
+++ src/main/java/org/veupathdb/service/userds/util/DbMan.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.postgresql.Driver;
 import org.veupathdb.service.userds.Main;
 
 public class DbMan
@@ -17,6 +18,7 @@ public class DbMan
 
   public static DataSource initImportDb() {
     final var config = new HikariConfig();
+    config.setDriverClassName(Driver.class.getName());
     config.setJdbcUrl(String.format(
       "jdbc:postgresql://%s:%d/postgres",
       Main.options.getDsHost().orElse(DEFAULT_HOST),
