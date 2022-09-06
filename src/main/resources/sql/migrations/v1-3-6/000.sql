8541d89e8d313223a868c95f3c46ab12f3d44cdf
Dan Galdi <dmgaldi@upenn.edu>
2022-09-06 09:50:48 -0400

    Add a migration to fix typo in TriTryp project name

---
+++ src/main/resources/sql/migrations/v1-3-6/000.sql
@@ -0,0 +1,10 @@
+DELETE FROM
+  ds_types.project
+WHERE name='TritrypDB'
+;
+
+INSERT INTO
+  ds_types.project (name)
+VALUES
+  ('TriTrypDB')
+;
---
+++ src/main/resources/sql/migrations/v1-3-6/999.sql
@@ -0,0 +1,7 @@
+UPDATE
+  ds_meta.kv_store
+SET
+    value = 'v1.3.6'
+WHERE
+    key = 'version'
+;
